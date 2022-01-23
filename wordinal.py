b71cee2138870b5c776f9bb626ae4c7611eaf5e6
Julian Giamblanco <giamblanco.julian@gmail.com>
2022-01-23 14:59:57 -0800

    Refactor #1
    - Add optional hard mode
    - Limit to english words
    - Pre-load list of words instead of iterating on each play

--- wordinal.py
+++ wordinal.py
@@ -1,14 +1,15 @@
-from typing import Optional
-from prompt_toolkit import prompt as prompt_unformatted, print_formatted_text, HTML
-from nltk.corpus import words
 from random import choice
+from typing import List, Optional
+
+from nltk.corpus import words
+from prompt_toolkit import HTML, print_formatted_text
+from prompt_toolkit import prompt as prompt_unformatted
 
 
-def five_letter_word():
-    while True:
-        word = choice(words.words())
-        if len(word) == 5:
-            return str(word).lower()
+def five_letter_words():
+    for word in words.words("en"):
+        if len(word) == 5 and word.lower() == word:
+            yield word
 
 
 def green_bg(text: str):
@@ -38,12 +39,14 @@ def ansi(color: str, text: str, *, bg: Optional[str] = None):
 class Game:
     LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
-    def __init__(self) -> None:
-        self.word = five_letter_word()
+    def __init__(self, valid_words: List[str], *, hard_mode=False) -> None:
+        self.valid_words = set(valid_words)
+        self.word = choice(valid_words)
         self.remaining_letters = set(self.LETTERS)
         self.guessed_letters = set()
         self.misplaced_letters = set()
         self.guesses = 0
+        self.hard_mode = hard_mode
 
     def format_guess(self, guess: str):
         output = ""
@@ -73,11 +76,24 @@ class Game:
                 output += " "
         return output
 
-    def play(self):
+    def get_guess(self):
+        return prompt(ansi("cyan", "Enter a word: "))
 
+    def guess_is_valid(self, guess: str):
+        if len(guess) != 5:
+            fprint(f"{ansi('red', guess)} is not 5 letters.")
+            return False
+        elif HARD and any(guessed_letters := [char for char in guess if char not in self.remaining_letters]):
+            fprint(
+                f"{ansi('cyan', 'Letters: ')}{ansi('red', ', '.join(guessed_letters))}{ansi('cyan', ' have already been guessed!')}"
+            )
+            return False
+        return True
+
+    def play(self):
         while self.guesses < 6:
-            while len(guess := prompt(ansi("cyan", "Enter a word: "))) != 5:
-                fprint(f"{ansi('red', guess)} is not 5 letters.")
+            while not self.guess_is_valid(guess := self.get_guess()):
+                pass
             fprint(ansi("cyan", "You said: ") + self.format_guess(guess))
             fprint(ansi("cyan", "Remaining letters: ") + self.letters())
             if guess == self.word:
@@ -88,5 +104,7 @@ class Game:
 
 
 if __name__ == "__main__":
+    hard_mode = "y" in prompt(ansi("cyan", "Play in hard mode? y/n: "))
+    words = list(five_letter_words())
     while True:
-        Game().play()
+        Game(words, hard_mode=hard_mode).play()
